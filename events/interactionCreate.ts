92712245d5879301921303993f73ceb68d573de7
minesettimi <minesettimi@gmail.com>
2023-10-21 23:38:58 -0400

    interaction fixing

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,12 +1,13 @@
-import { Interaction } from "discord.js";
+import { Interaction, InteractionType } from "discord.js";
 
 export = {
     name: 'interactionCreate',
     async execute(interaction : Interaction) {
         if (!interaction.isChatInputCommand()) return;
 
-        const command = interaction.client.commands.get(interaction.commandName);
+        if (interaction.type !== InteractionType.ApplicationCommand) return;
 
+        const command = interaction.client.commands.get(interaction.commandName);
 
         if (!command) return;
 
